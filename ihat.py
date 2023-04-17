2b917cb04f7471f8e702d395fb2a6a19be34d590
Pradhyum R <drpradhyum2016@outlook.com>
2023-04-17 15:49:11 +0000

    added more assessment features

--- ihat.py
+++ ihat.py
@@ -10,27 +10,27 @@ np.seterr(all="ignore")
 init_printing()
 
 vs, entries, data = fake_force_data()
-
-dv = symbols('a')           # Trying to predict petal length
-using = symbols('m F')     # Using sepal length and width
+dv = symbols('F')           # Trying to predict petal length
+using = symbols('m a')     # Using sepal length and width
 
 def get_all_mutations(expr):   # Very simple for now
     for x in using:
         yield expr + x
         yield expr - x
-        yield expr * x
-        yield expr / x
+        yield expand(expr * x)
+        yield expand(expr / x)
 
 def assess(expr):       # Lower is better
     f = lambdify(vs, expr, "numpy")
     o = f(*entries)
-    accuracy   = np.nanvar(data[dv] / o) + np.nanvar(o / data[dv])
-    complexity = sum(map(expr.count, vs))
-    return accuracy + complexity**2
+    accuracy     = np.nanvar(data[dv] / o) + np.nanvar(o / data[dv])
+    completeness = sum(min(expr.count(v), 1) for v in vs)
+    complexity   = sum(map(expr.count, vs))
+    return accuracy + max(complexity - completeness, 0)**2
 
-gen = [Integer(1)]
+gen = [*using]
 temp_store = []
-for _ in range(5):     # Number of generations
+for _ in range(10):     # Number of generations
     for expr in gen:
         heapq.heappush(temp_store, container(assess(expr), expr))
     l = [x.expr for x in temp_store[:10]]
@@ -38,3 +38,6 @@ for _ in range(5):     # Number of generations
     gen = chain(l, chain.from_iterable(map(get_all_mutations, l)))
     temp_store = []
 print(l)
+
+m, a = symbols('m a')
+print(assess(m*a))
