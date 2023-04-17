0d1ad02d9a1656587bfba1c6f86281eb3a7e11eb
Pradhyum R <drpradhyum2016@outlook.com>
2023-04-17 13:47:48 +0000

    removed var counting logic (not necessary)

--- utils.py
+++ utils.py
@@ -5,19 +5,3 @@ from typing import Any
 class container:
     assessment: float
     expr: Any = field(compare=False)
-
-class var_counter:
-    def __init__(self, v=1):
-        self.v = v
-
-    def __repr__(self): return f"var_counter({self.v})"
-    def __str__(self):  return f"var_counter({self.v})"
-
-    def _prototype(self, other):
-        if type(other) == type(self):
-            return var_counter(self.v + other.v)
-        return self
-
-for x in dir(int):
-    if x.startswith('__') and x not in dir(var_counter):
-        setattr(var_counter, x, var_counter._prototype)
