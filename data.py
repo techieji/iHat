135ff5c7ada87c22f2a61f20966b83bb0eed3aeb
Pradhyum R <drpradhyum2016@outlook.com>
2023-04-26 01:42:10 +0000

    Added generic data generator (untested)

--- data.py
+++ data.py
@@ -3,12 +3,14 @@ from numpy.random import default_rng
 from sympy import *
 import csv
 from random import randint
+from typing import Callable
 
 rng = default_rng()
 
 def get_iris():
     with open('iris.data') as iris:
         _vs, *_data = list(csv.reader(iris))
+        # _vs, *_data = iris |> csv.reader |> list
         vs = symbols(_vs[:-1])               # Going to be used to actually evaluate a hypothesis
         entries = np.array([x[:-1] for x in _data], dtype=np.float32).T    # Removing class (meant for continuous data)
         data = dict(zip(vs, entries))
@@ -25,6 +27,16 @@ def fake_force_data(datasize=210):     # a = F/m    =>    F = ma
     acceleration = force/masses + rng.normal(size=datasize)/10
     vs = symbols('m F a')
     entries = np.array([masses, force, acceleration])
+    data = dict(zip(vs, entries))
+    return vs, entries, data
+
+def from_function(fn: Callable[[np.ndarray, ...], np.ndarray], output_sym: Symbol, ranges: dict[Symbol, tuple[float, float]], datasize=210, error=True):
+    syms = ranges.keys()
+    errors = rng.normal(size=datasize) else np.zeros(datasize)
+    _entries = [rng.uniform(lower, upper, size=datasize) for lower, upper in ranges.values()]
+    res = fn(*_entries) + errors
+    entries = np.column_stack(_entries + [res])
+    vs = list(syms) + [output_sym]
     data = dict(zip(vs, entries))
     return vs, entries, data
 
